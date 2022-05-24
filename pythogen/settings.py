d4b8fd2232a280f3ff82054e955366f22eee55dc
artsmolin <artursmolin@outlook.com>
2022-05-24 15:22:28 +0300

    0.0.15

--- pythogen/settings.py
+++ pythogen/settings.py
@@ -4,4 +4,4 @@ from pathlib import Path
 CLIENT_TEMPLATE_NAME = "httpx.j2"
 
 CURRENT_DIR_PATH = Path(__file__).parent.absolute()
-TEMPLATES_DIR_PATH = CURRENT_DIR_PATH / Path("pythogen/templates")
+TEMPLATES_DIR_PATH = CURRENT_DIR_PATH / Path("templates")
