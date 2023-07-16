55ffbb9cd2ca77ef6b64c19d8964e68e0985a0c1
artsmolin <artursmolin@outlook.com>
2023-07-16 10:36:30 +0300

    Refactoring

--- pythogen/settings.py
+++ pythogen/settings.py
@@ -5,4 +5,4 @@ CLIENT_TEMPLATE_NAME = "main.j2"
 
 CURRENT_DIR_PATH = Path(__file__).parent.absolute()
 HTTP_CLIENT_TEMPLATES_DIR_PATH = CURRENT_DIR_PATH / Path("templates/http_client")
-HTTP_PACKAGE_TEMPLATES_DIR_PATH = CURRENT_DIR_PATH / Path("templates/package")
+HTTP_PACKAGE_TEMPLATES_DIR_PATH = CURRENT_DIR_PATH / Path("templates/http_client_package")
