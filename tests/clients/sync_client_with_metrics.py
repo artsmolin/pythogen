9a23ce2f5296b659823d57011b3e7bcae047cb28
artsmolin <artursmolin@outlook.com>
2023-10-27 13:23:13 +0300

    Refactoring

--- tests/clients/sync_client_with_metrics.py
+++ tests/clients/sync_client_with_metrics.py
@@ -133,7 +133,7 @@ class DefaultLogsIntegration:
         return {"props": {"data": kwargs}}
 
     def log_error(self, req: RequestBox, resp: ResponseBox) -> None:
-        msg = "request error"
+        msg = f"request error"
         msg += f" | client={req.client_name}"
         msg += f" | method={req.method}"
         msg += f" | url={req.url}"
@@ -2655,7 +2655,7 @@ class Client:
         return {"extra": {"props": {"data": kwargs}}}
 
     def log_error(self, client_name: str, method, url: str, params, content, headers) -> None:
-        msg = "request error"
+        msg = f"request error"
         msg += f" | client={client_name}"
         msg += f" | method={method}"
         msg += f" | url={url}"
